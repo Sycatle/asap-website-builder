42a6dd333465213a294483d5a6507de8b9c71d79
sycatle <sycatle@users.noreply.github.com>
2025-12-14 16:09:07 +0100

    feat: enhance sheet component animations and add tailwindcss-animate plugin

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -137,6 +137,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animate')
 ],
 }
