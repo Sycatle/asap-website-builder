7df563c7998541560e097cf73193e9cd97e26c53
sycatle <sycatle@users.noreply.github.com>
2025-12-29 08:09:54 +0100

    feat(renderers): add Landing SaaS renderer
    - Create landing-saas-renderer for SaaS landing pages
    - Export new renderer from package index

--- packages/renderers/src/index.ts
+++ packages/renderers/src/index.ts
@@ -14,3 +14,6 @@ export * from './renderers';
 
 // V1 MVP: Freelance Portfolio Renderer
 export * from './freelance-renderer';
+
+// Landing Page SaaS Renderer
+export * from './landing-saas-renderer';
